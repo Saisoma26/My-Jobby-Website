2702946200214e5764f1003300d9e49ab9c38d07
Saisoma26 <saiprasannasoma26@gmail.com>
2025-01-25 15:51:37 -0500

    Switch to bcryptjs

--- auth-backend/routes/auth.js
+++ auth-backend/routes/auth.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 const jwt  = require('jsonwebtoken');
 const User = require('../models/User');
 
@@ -40,7 +40,7 @@ router .post('/login', async (req, res) => {
             return res.status(400).json({message:"user not found"});
         }
 
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = await bcryptjs.compare(password, user.password);
         if(!isMatch){
             return res.status(400).json({message:"incorrect passowrd"})
         }
