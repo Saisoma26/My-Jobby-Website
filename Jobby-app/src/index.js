293091db24b2fcf37c79e4b8aeef1f2cacb6343c
Saisoma26 <saiprasannasoma26@gmail.com>
2025-01-26 14:49:01 -0500

    changed paths reverted

--- Jobby-app/src/index.js
+++ Jobby-app/src/index.js
@@ -6,7 +6,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/My-Jobby-Website">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
